bca0dbd4039a4bbcf7bba577457b4f20298fb671
lijo <lijomon24@gmail.com>
2024-04-09 10:07:08 +0100

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,65 @@
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using System.Diagnostics;
+using NuGet.Frameworks;
+
+namespace Csharpprojectselenium
+{
+    public class locators
+    {
+
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.Progress.WriteLine("this is setup");
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            TestContext.Progress.WriteLine("this is test1");
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+            TestContext.Progress.WriteLine(driver.Url);
+            TestContext.Progress.WriteLine(driver.Title);
+
+
+            //driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacadem");
+            driver.FindElement(By.Name("password")).SendKeys("learning");
+            driver.FindElement(By.CssSelector("input[type='checkbox'")).Click();
+            driver.FindElement(By.XPath("//input[@id='signInBtn']")).Click();
+
+            // to collect error message give wrong username
+            string er_message = driver.FindElement(By.ClassName("alert-danger")).Text;
+            TestContext.Progress.WriteLine(er_message);
+
+            IWebElement Link_1 = driver.FindElement(By.LinkText("Free Access to InterviewQues/ResumeAssistance/Material"));
+            String actual_attr = Link_1.GetAttribute("href");
+            String expected_attr = "https://rahulshettyacademy.com/documents-request";
+            Assert.That(actual_attr, Is.EqualTo(expected_attr));
+
+
+
+
+        }
+
+        [TearDown]
+        public void close()
+        {
+            TestContext.Progress.WriteLine("this is teardown");
+            driver.Close();
+        }
+    }
+}
